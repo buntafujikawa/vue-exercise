76c7d85bff6088e121901c7c6406ff5b1fc492b9
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-04-29 19:46:10 +0900

    Vuejs Tutorial #8 - Vue Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Account from '@/components/Account'
+import Contact from '@/components/Contact'
+import Friends from '@/components/Friends'
 
 Vue.use(Router)
 
@@ -8,8 +11,24 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
+      name: 'Hello',
       component: HelloWorld
+    },
+    {
+      path: '/friends/:id/:age/:weight',
+      name: 'Friends',
+      props: true,
+      component: Friends
+    },
+    {
+      path: '/contact',
+      name: 'Contact',
+      component: Contact
+    },
+    {
+      path: '/account',
+      name: 'Account',
+      component: Account
     }
   ]
 })
