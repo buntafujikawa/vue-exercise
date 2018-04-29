6568f2d5ecdfbb5b82761c0c56eacb7254f2c5fa
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-04-29 11:35:10 +0900

    Vue Tutorial # 2 - Vue.js filters and computed data

--- src/main.js
+++ src/main.js
@@ -13,20 +13,41 @@ new Vue({
   components: {App},
   data: {
     bobby: {
-      name: 'Bobby Bone',
+      first: 'Bobby',
+      last: 'Boone',
       age: 25
     },
     john: {
-      name: 'John Bobby',
+      first: 'John',
+      last: 'Bobby',
       age: 35
+    }
+  },
+  computed: {
+    // bobbyFullName () {
+    //   return `${this.bobby.first} ${this.bobby.last}`
+    // },
+    // johnFullName () {
+    //   return `${this.john.first} ${this.john.last}`
+    // },
+    // johnAgeInOneYear () {
+    //   return this.john.age + 1
+    // }
+  },
+  filters: {
+    ageInOneYear (age) {
+      return age + 1
+    },
+    fullName (value) {
+      return `${value.first} ${value.last}`
     }
   },
   template: `
     <div>
-        <h2>Name: {{john.name}}</h2>
-        <h2>Age: {{john.age}}</h2>
-        <h2>Name: {{bobby.name}}</h2>
-        <h2>Age: {{bobby.age}}</h2>
+        <h2>Name: {{john | fullName}}</h2>
+        <h2>Age: {{john.age | ageInOneYear}}</h2>
+        <h2>Name: {{bobby | fullName}}</h2>
+        <h2>Age: {{bobby.age | ageInOneYear}}</h2>
     </div>
 `
 })
