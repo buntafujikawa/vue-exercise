c4772f8a9f152dabb2ec6b554071c305afcca024
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-07-21 11:44:16 +0900

    add video operation button

--- youtube/src/components/HelloWorld.vue
+++ youtube/src/components/HelloWorld.vue
@@ -1,113 +1,56 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div id="#app">
+    <section>
+      <h2>listening events</h2>
+      <youtube id="player" :video-id="videoId" @ready="ready" @playing="playing"></youtube>
+      <button @click="play()">play</button>
+      <button @click="stop()">stop</button>
+      <button @click="pause()">pause</button>
+      <button @click="seek(5)">seek</button>
+      <button @click="clear(5)">clear</button>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      // https://www.youtube.com/watch?v=rfi3w9Bzwik
+      videoId: 'rfi3w9Bzwik'
+    }
+  },
+  methods: {
+    ready (event) {
+      this.player = event.target
+    },
+    playing (event) {
+      // The player is playing a video.
+      // event.target.playing()
+    },
+    change () {
+      // when you change the value, the player will also change.
+      // If you would like to change `playerVars`, please change it before you change `videoId`.
+      // If `playerVars.autoplay` is 1, `loadVideoById` will be called.
+      // If `playerVars.autoplay` is 0, `cueVideoById` will be called.
+      this.videoId = 'another video id'
+    },
+    // see https://developers.google.com/youtube/iframe_api_reference?hl=ja
+    play () {
+      this.player.playVideo()
+    },
+    stop () {
+      this.player.stopVideo()
+    },
+    pause () {
+      this.player.pauseVideo()
+    },
+    seek (second) {
+      this.player.seekTo(second)
+    },
+    clear () {
+      this.player.clearVideo()
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
