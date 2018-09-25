a919714203c64de6f4219143e661e9c340b5a07c
buntafujikawa <bunta.fujikawa@mkt-apps.com>
2018-09-25 19:14:18 +0900

    add render template

---
+++ basic/src/components/Render.vue
@@ -0,0 +1,22 @@
+<script>
+  export default {
+    data () {
+      return {
+        level: 2,
+        slots: 'hoge'
+      }
+    },
+    render: function (createElement) {
+      return createElement(
+        'h' + this.level,   // タグ名
+        this.slots
+      )
+    },
+    // props: {
+    //   level: {
+    //     type: Number,
+    //     required: true
+    //   }
+    // }
+  }
+</script>
